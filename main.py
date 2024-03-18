5427e41223eda388b389655b53ad50f62f1db8ea
Sahar <saharujan@gmail.com>
2024-03-18 16:50:32 -0400

    object detection on a video

--- main.py
+++ main.py
@@ -1,16 +1,67 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import torch
+import torchvision
+from torchvision import transforms as T
+import cv2
+import cvzone
+
+"""Create the model"""
+model = torchvision.models.detection.ssd300_vgg16(weights=True)
+
+model.eval()
+"""class names"""
+classnames = []
+with open('classes.txt', 'r') as f:
+    classnames = f.read().splitlines()
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+"""load video"""
+# Create a VideoCapture object and read from input file
+cap = cv2.VideoCapture('cars2.mp4')
+# Check if camera opened successfully
+if (cap.isOpened() == False):
+  print("Error opening video file")
+
+
+  # Read until video is completed
+while (cap.isOpened()):
+
+  # Capture frame-by-frame
+  ret, frame = cap.read()
+  if ret == True:
+
+
+    # Display the resulting frame
+
+    img = frame.copy()
+    # print(type(img))
+    # convert it to a tensor
+    imgtranform = T.ToTensor()
+    image = imgtranform(frame)
+    # print(type(image))
+    with torch.no_grad():
+      ypred = model([image])
+      # print(ypred[0].keys())
+      bbox, scores, labels = ypred[0]['boxes'], ypred[0]['scores'], ypred[0]['labels']
+      nums = torch.argwhere(scores > 0.80).shape[0]
+      for i in range(nums):
+        x, y, w, h = bbox[i].numpy().astype('int')
+        cv2.rectangle(img, (x, y), (w, h), (0, 0, 255), 3)
+        classname = labels[i].numpy().astype('int') - 1
+        classdetected = classnames[classname]
+        print(classdetected)
+        cvzone.putTextRect(img, classdetected, [x, y - 20], scale=2, border=2)
+
+    cv2.imshow('DFrame', img)
+    # cv2.imshow('Frame', frame)
+
+    # Press Q on keyboard to exit
+    if cv2.waitKey(25) & 0xFF == ord('q'):
+      break
+
+  # Break the loop
+  else:
+    break
--- pytorch_SSD.py
+++ pytorch_SSD.py
@@ -1,51 +1,42 @@
-"""Ref:https://github.com/Tech-Watt/YOUTUBE-TUTORIAL-CODES """
-
-
 import torch
 import torchvision
 from torchvision import transforms as T
 import cv2
 import cvzone
 
-"""Create the model"""
-model = torchvision.models.detection.ssd300_vgg16(weights=True)
 
+model = torchvision.models.detection.ssd300_vgg16(pretrained = True)
 model.eval()
-"""class names"""
+
+
 classnames = []
-with open('classes.txt', 'r') as f:
+with open('classes.txt','r') as f:
     classnames = f.read().splitlines()
 
 # print(classnames[0])
-"""Read the image"""
-image = cv2.imread('dog1.jpg') # it is a numpy array
-img = image.copy()
-# convert it to a tensor
-imgtranform = T.ToTensor()
-image = imgtranform(image)
-# print(type(image))
 
-"""Make a prediction using the model"""
+image = cv2.imread('dog1.jpg')
+print(type(image))
+img = image.copy()
+# print(type(img))
+#
+imgtransform = T.ToTensor()
+image = imgtransform(image)
+print(type(image))
+
 with torch.no_grad():
     ypred = model([image])
     print(ypred[0].keys())
 
-    bbox,scores,labels = ypred[0]['boxes'], ypred[0]['scores'], ypred[0]['labels']
+    bbox,scores,labels = ypred[0]['boxes'],ypred[0]['scores'],ypred[0]['labels']
     nums = torch.argwhere(scores > 0.80).shape[0]
-    print(nums)
     for i in range(nums):
         x,y,w,h = bbox[i].numpy().astype('int')
-        cv2.rectangle(img, (x,y), (w,h), (0,0,255),5)
-        classname = (labels[i].numpy().astype('int'))-1
+        cv2.rectangle(img,(x,y),(w,h),(0,0,255),5)
+        classname = labels[i].numpy().astype('int')-1
         classdetected = classnames[classname-1]
-        cvzone.putTextRect(img,classdetected,[x,y-10], scale=2, border=2)
+        # print(classdetected)
+        cvzone.putTextRect(img,classdetected,[x,y-20],scale=2,border=2)
 
-
-
-
-cv2.imshow('frame', img)
+cv2.imshow('frame',img)
 cv2.waitKey(0)
-
-
-
-
